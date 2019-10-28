0250411c1614a0b00a77ccd47cbf1fc3b8fe8279
Morgan McMilin <mmcmilin@yahoo.com>
2019-10-27 19:29:50 -0700

    changed background and the button types

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -4,19 +4,19 @@ $(document).ready(function () {
     // Creating Functions & Methods
     // Function that displays all gif buttons
     function displayGifButtons() {
-        $("#gifButtonsView").empty(); // erasing anything in this div id so that it doesnt duplicate the results
+        $("#buttonsView").empty(); // erasing anything in this div id so that it doesnt duplicate the results
         for (var i = 0; i < actions.length; i++) {
             var gifButton = $("<button>");
             gifButton.addClass("action");
-            gifButton.addClass("btn btn-primary")
+            gifButton.addClass("btn btn-success")
             gifButton.attr("data-name", actions[i]);
             gifButton.text(actions[i]);
-            $("#gifButtonsView").append(gifButton);
+            $("#buttonsView").append(gifButton);
         }
     }
     // Function to add a new action button
     function addNewButton() {
-        $("#addGif").on("click", function () {
+        $("#gifAdd").on("click", function () {
             var action = $("#action-input").val().trim();
             if (action == "") {
                 return false; // added so user cannot add a blank button
@@ -31,7 +31,7 @@ $(document).ready(function () {
     // Doesnt work properly yet removes all of the added buttons
     // rather than just the last
     function removeLastButton() {
-        $("removeGif").on("click", function () {
+        $("gifRemove").on("click", function () {
             actions.pop(action);
             displayGifButtons();
             return false;
